fb6148f2c8a332ffaec6a7805d883c6f85ce28be
David Miller <davidmiller@Jennifers-MacBook-Pro.local>
2015-01-04 14:00:16 -0600

    Initial Commit

---
+++ TaxCalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculatorApp
+//
+//  Created by David Miller on 1/4/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
